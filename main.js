51523be1fbf24b92f5bed2cdf6fa185566261e17
Jack25440 <jack.b.richards@bigpond.com>
2018-03-03 14:19:00 +1000

    Adding stuff

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const config = require("./config.json"),
 
 bot.on("ready", () => {
     console.log("Bot logged in");
+})
+;
+
+bot.on("message", message => {
+    // Do stuff
+    if (message.content.startsWith(config.prefix)) {
+    // Commands
 });
 
 bot.login(config.token);
